cb808a29de823210369c45e3ab6362e84f1e01bb
muneebsafdar <safdarmuneeb95@gmail.com>
2025-10-31 23:44:25 +0500

    Fix nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,6 @@ const Navbar = () => {
     { name: 'About', href: '#about' },
     { name: 'Skills', href: '#skills' },
     { name: 'Projects', href: '#projects' },
-    { name: 'Achievements', href: '#achievements' },
     { name: 'Contact', href: '#contact' },
   ];
 
